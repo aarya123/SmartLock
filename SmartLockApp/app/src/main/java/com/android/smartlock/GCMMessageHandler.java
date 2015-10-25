47a4a2546f3effa1101927e47767328e39a04a55
Dan Green <dan@Dans-MacBook-Pro.local>
2015-10-25 19:32:20 -0400

    Initial upload of Android mobile application

---
+++ SmartLockApp/app/src/main/java/com/android/smartlock/GCMMessageHandler.java
@@ -0,0 +1,100 @@
+package com.android.smartlock;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+public class GCMMessageHandler extends IntentService {
+    public static final int NOTIFICATION_ID = 1;
+    private static final String TAG = "msghandlerGCM";
+    NotificationCompat.Builder builder;
+    private NotificationManager mNotificationManager;
+
+    public GCMMessageHandler() {
+        super("GCMMessageHandler");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        Log.d(TAG, "Processing Notification!");
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM
+             * will be extended in the future with new message types, just ignore
+             * any message types you're not interested in, or that you don't
+             * recognize.
+             */
+            if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                sendNotification("Send error: " + extras.toString(), extras);
+            } else if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_DELETED.equals(messageType)) {
+                sendNotification("Deleted messages on server: " +
+                        extras.toString(), extras);
+                // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                // Post notification of received message.
+                sendNotification("Event Coming Up Soon!", extras);
+                Log.i(TAG, "Received: " + extras.toString());
+            }
+        }
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GCMReceiver.completeWakefulIntent(intent);
+    }
+
+    // Put the message into a notification and post it.
+    // This is just one simple example of what you might choose to do with
+    // a GCM message.
+    private void sendNotification(String msg, Bundle extras) {
+
+        Log.d(TAG, "sending notification!");
+       /* mNotificationManager = (NotificationManager)
+          this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle("GCM Notification")
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());*/
+        if (extras.getString("mode").equals("update_event"
+        )) {
+            String event_id = extras.getString("response");
+            Intent intent = new Intent(this, MainActivity.class);
+            Log.d("campusfeed", "creating pending intent with event id " + event_id);
+            //MainActivity.event_id = Long.parseLong(event_id);
+            intent.putExtra("update", event_id);
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+            Notification notification = new Notification.Builder(this).setContentTitle(this.getResources().getString(R.string.app_name))
+                    .setContentText(msg).setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pIntent).getNotification();
+            NotificationManager notificationManager = (NotificationManager) this.getSystemService(this.NOTIFICATION_SERVICE);
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+            notificationManager.notify(0, notification);
+
+        } else {
+            // invite event notification
+        }
+
+
+    }
+}
---
+++ SmartLockApp/app/src/main/java/com/android/smartlock/Internet.java
@@ -0,0 +1,59 @@
+package com.android.smartlock;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Internet extends AsyncTask<String, String, String> {
+
+    int RESPONSE_LEN = 2048;
+
+    private String tcpSend(String content) {
+        final String header = "GET /smartlock-user-agent/ HTTP/1.1\n" +
+                "Host: smartlock\n" +
+                "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n" +
+                "Content-Length: " + content.length() + "\n" +
+                "Accept-Language: en-us,en;q=0.5\n\n";
+        String msg = header + content + '\n';
+
+        Log.d("TCP", Constants.IP + " " + Constants.PORT + " " + Constants.TIMEOUT + " " + content);
+
+        char[] buff = new char[RESPONSE_LEN];
+        String modifiedSentence;
+        Socket clientSocket;
+        try {
+            clientSocket = new Socket(Constants.IP, Constants.PORT);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToServer.writeBytes(msg);
+            clientSocket.setSoTimeout(Constants.TIMEOUT);
+            inFromServer.read(buff, 0, RESPONSE_LEN);
+            modifiedSentence = String.valueOf(buff).trim();
+            clientSocket.close();
+            outToServer.close();
+            inFromServer.close();
+        } catch (Exception exc) {
+            modifiedSentence = "";
+        }
+        return modifiedSentence;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String get = "";
+        for (int i = 0; i < params.length; i += 2) {
+            get += params[i] + "=" + params[i + 1] + "\n";
+        }
+        return tcpSend(get);
+    }
+
+    protected void onPostExecute(String response) {
+        Log.d("TCP Recieved: ", response);
+    }
+
+}
