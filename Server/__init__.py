3b2c973fe30b69021fb65fa8df4d84a80ad3a795
Dan Green <green131@purdue.edu>
2015-10-27 23:09:57 -0400

    Real commit of new server stuffs

--- Server/__init__.py
+++ Server/__init__.py
@@ -1,1 +1,8 @@
-__author__ = 'dan'
+import logging
+
+from processing import launch_tcp_server
+
+
+if __name__ == '__main__':
+    logging.info('Launching SmartLock Server...')
+    launch_tcp_server()
