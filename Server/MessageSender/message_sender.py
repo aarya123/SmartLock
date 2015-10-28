1f1c2d0c09ffd5b746b8a9d61d4fc0133cb1a430
Dan Green <green131@purdue.edu>
2015-10-27 23:06:43 -0400

    Refactored server and implemented fixed classes for message handling and sending. Fix for broken pipe error, added proper error handling, and added proper socket closures.

---
+++ Server/MessageSender/message_sender.py
@@ -0,0 +1,26 @@
+import logging
+import socket
+
+
+class MessageSender:
+    server = None
+
+    def __init__(self, server, ):
+        self.server = server
+
+    def __call__(self, msg, ):
+        logging.debug('Sender - Send msg: {}'.format(msg))
+        try:
+            self.server.wfile.write(msg)
+            logging.debug('Sender - Sent msg')
+        except socket.error, e:
+            logging.error('Sender - Failed to send response: {}'.format(e))
+
+    def __del__(self, ):
+        logging.debug('Sender - Closing socket')
+        try:
+            if not self.server.wfile.closed:
+                self.server.wfile.flush()
+                self.server.wfile.close()
+        except socket.error, e:
+            logging.error('Sender - Failed to close socket: {}'.format(e))
