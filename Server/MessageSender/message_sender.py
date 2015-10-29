9f651e506566c435dad0e1ad5b4116165ef70555
green131 <green131@purdue.edu>
2015-10-29 19:22:29 -0400

    Made logging beautiful. Created database manager class

--- Server/MessageSender/message_sender.py
+++ Server/MessageSender/message_sender.py
@@ -3,24 +3,27 @@ import socket
 
 
 class MessageSender:
+    log = None
     server = None
 
     def __init__(self, server, ):
+        self.log = logging.getLogger('MessageSender')
+        self.log.setLevel(logging.DEBUG)
         self.server = server
 
     def __call__(self, msg, ):
-        logging.info('msg_response: {}'.format(msg))
+        self.log.info('msg_response: {}'.format(msg))
         try:
             self.server.wfile.write(msg)
-            logging.debug('Sender - Sent msg')
+            self.log.debug('Sent msg')
         except socket.error, e:
-            logging.error('Sender - Failed to send response: {}'.format(e))
+            self.log.error('Failed to send response: {}'.format(e))
 
     def __del__(self, ):
-        logging.debug('Sender - Closing write socket')
+        self.log.debug('Closing write socket')
         try:
             if not self.server.wfile.closed:
                 self.server.wfile.flush()
                 self.server.wfile.close()
         except socket.error, e:
-            logging.error('Sender - Failed to close socket: {}'.format(e))
+            self.log.error('Failed to close socket: {}'.format(e))
