1efaed8ad24fa9f6c52dfb364a643fd8902b0e00
green131 <green131@purdue.edu>
2015-11-08 18:29:40 -0500

    Fixed lock constants

--- Server/constants.py
+++ Server/constants.py
@@ -1,6 +1,6 @@
 # RPiHandler constants
-LOCKED = 0
-UNLOCKED = 1
+LOCKED = 1
+UNLOCKED = 0
 
 ALREADY_REGISTERED_MSG = 'You are already registered with this lock!'
 REGISTRATION_SUCCESS_MSG = 'You have been successfully registered! :)'
