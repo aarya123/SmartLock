809d8063601c67d1fd7fd47878f568a5f6db2184
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-11-08 18:32:51 -0500

    Gutted PiPinger

--- SmartLockApp/app/src/main/java/com/android/smartlock/Internet/PiPinger.java
+++ SmartLockApp/app/src/main/java/com/android/smartlock/Internet/PiPinger.java
@@ -13,26 +13,14 @@ public class PiPinger implements Runnable {
 
     @Override
     public void run() {
-        boolean result;
-        boolean viewNeedsUpdating = false;
         try {
             String[] response = new Internet(Constants.getIPAdress(), "ping", "true").getResult().split("\n");
-            result = response[0].contains("pong");
-            boolean lock = Boolean.parseBoolean(response[1].split("=")[1]);
-            if (lock != lockStatus) {
-                viewNeedsUpdating = true;
-            }
-            lockStatus = lock;
+            mPiVisible = response[0].contains("pong");
+            lockStatus = Boolean.parseBoolean(response[1].split("=")[1]);
         } catch (Exception e) {
-            result = false;
+            mPiVisible = false;
         }
-        if (result != mPiVisible) {
-            viewNeedsUpdating = true;
-        }
-        mPiVisible = result;
-        if (viewNeedsUpdating) {
-            mAsyncTaskListener.onAsyncTaskCompleted();
-        }
+        mAsyncTaskListener.onAsyncTaskCompleted();
     }
 
     public boolean isPiVisible() {
