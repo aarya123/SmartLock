39cc7ae20146f8a2bfdde9643af974a8c1713b58
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-12-06 18:31:56 -0500

    Base work on public key sending

--- SmartLockApp/app/src/main/java/com/android/smartlock/Constants.java
+++ SmartLockApp/app/src/main/java/com/android/smartlock/Constants.java
@@ -3,6 +3,13 @@ package com.android.smartlock;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Base64;
+
+import java.math.BigInteger;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 
 public class Constants {
 
@@ -11,12 +18,43 @@ public class Constants {
     private static final String PORT_KEY = "port";
     private static final String TIMEOUT_KEY = "timeout";
     private static final String APPROVED_KEY = "approved";
+    private static final String PUBLIC_KEY_MODULUS_KEY = "pbkm";
+    private static final String PRIVATE_KEY_MODULUS_KEY = "prkm";
+    private static final String PUBLIC_KEY_EXPONENT_KEY = "pbke";
+    private static final String PRIVATE_KEY_EXPONENT_KEY = "prke";
+
     private static SharedPreferences mSharedPreferences;
     private static String IP_ADDRESS;
     private static String DEVICE_ID;
     private static int PORT;
     private static int TIMEOUT;
     private static boolean APPROVED;
+    private static PublicKey PUBLIC_KEY;
+    private static PrivateKey PRIVATE_KEY;
+
+    private static void initRSA() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(1024);
+            KeyPair kp = kpg.genKeyPair();
+            KeyFactory fact = KeyFactory.getInstance("RSA");
+            RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(), RSAPublicKeySpec.class);
+            RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(), RSAPrivateKeySpec.class);
+            BigInteger public_key_modulus = pub.getModulus();
+            BigInteger private_key_modulus = priv.getModulus();
+            BigInteger public_key_exponent = pub.getPublicExponent();
+            BigInteger private_key_exponent = priv.getPrivateExponent();
+            PUBLIC_KEY = kp.getPublic();
+            PRIVATE_KEY = kp.getPrivate();
+            mSharedPreferences.edit()
+                    .putString(PUBLIC_KEY_MODULUS_KEY, public_key_modulus.toString())
+                    .putString(PRIVATE_KEY_MODULUS_KEY, private_key_modulus.toString())
+                    .putString(PUBLIC_KEY_EXPONENT_KEY, public_key_exponent.toString())
+                    .putString(PRIVATE_KEY_EXPONENT_KEY, private_key_exponent.toString()).apply();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void init(Context context) {
         if (mSharedPreferences == null) {
@@ -26,7 +64,36 @@ public class Constants {
             PORT = mSharedPreferences.getInt(PORT_KEY, 8000);
             TIMEOUT = mSharedPreferences.getInt(TIMEOUT_KEY, 5000);
             APPROVED = mSharedPreferences.getBoolean(APPROVED_KEY, false);
+            BigInteger public_key_modulus = new BigInteger(mSharedPreferences.getString(PUBLIC_KEY_MODULUS_KEY, "-1"));
+            BigInteger private_key_modulus = new BigInteger(mSharedPreferences.getString(PRIVATE_KEY_MODULUS_KEY, "-1"));
+            BigInteger public_key_exponent = new BigInteger(mSharedPreferences.getString(PUBLIC_KEY_EXPONENT_KEY, "-1"));
+            BigInteger private_key_exponent = new BigInteger(mSharedPreferences.getString(PRIVATE_KEY_EXPONENT_KEY, "-1"));
+            if (public_key_modulus.equals(new BigInteger("-1"))) {
+                initRSA();
+            } else {
+                try {
+                    KeyFactory fact = KeyFactory.getInstance("RSA");
+                    PUBLIC_KEY = fact.generatePublic(new RSAPublicKeySpec(public_key_modulus, public_key_exponent));
+                    PRIVATE_KEY = fact.generatePrivate(new RSAPrivateKeySpec(private_key_modulus, private_key_exponent));
+                } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    public static PublicKey getPublicKey() {
+        return PUBLIC_KEY;
+    }
+
+    public static String getPublicKeyString() {
+        return "-----BEGIN PUBLIC KEY-----&&&"
+                + Base64.encodeToString(PUBLIC_KEY.getEncoded(), Base64.DEFAULT).replace("\n", "&&&")
+                + "&&&-----END PUBLIC KEY-----";
+    }
+
+    public static PrivateKey getPrivateKey() {
+        return PRIVATE_KEY;
     }
 
     public static String getDeviceId() {
