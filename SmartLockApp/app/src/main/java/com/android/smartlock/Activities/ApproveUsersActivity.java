de2687c08079df0c7700fd3835dd40b46fe1a9f4
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-12-06 15:10:06 -0500

    ListView setup

--- SmartLockApp/app/src/main/java/com/android/smartlock/Activities/ApproveUsersActivity.java
+++ SmartLockApp/app/src/main/java/com/android/smartlock/Activities/ApproveUsersActivity.java
@@ -2,13 +2,16 @@ package com.android.smartlock.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import com.android.smartlock.R;
 
 public class ApproveUsersActivity extends AppCompatActivity {
+    ListView approveUsersListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_approve_users);
+        approveUsersListView = (ListView) findViewById(R.id.approveUsersListView);
     }
 }
