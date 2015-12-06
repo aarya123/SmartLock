20c21fdb1d7ded0025e799ba512f6df56572e914
green131 <green131@purdue.edu>
2015-12-06 14:49:50 -0500

    Added approval process to server

--- Server/constants.py
+++ Server/constants.py
@@ -2,5 +2,7 @@
 LOCKED = 1
 UNLOCKED = 0
 
-ALREADY_REGISTERED_MSG = 'You are already registered with this lock!'
-REGISTRATION_SUCCESS_MSG = 'You have been successfully registered! :)'
+ADMIN_SUCCESS_MSG = 'You have been added as an admin.'
+ALREADY_REGISTERED_MSG = 'You are already registered with this lock'
+REGISTRATION_PENDING_MSG = 'Registration pending approval.'
+REGISTRATION_SUCCESS_MSG = 'Registration approved!'
