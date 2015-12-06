78cd177d06b4748556c7f11d18057621382fbb81
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-12-06 15:00:20 -0500

    ApproveUsersActivity

---
+++ SmartLockApp/app/src/main/java/com/android/smartlock/Activities/ApproveUsersActivity.java
@@ -0,0 +1,14 @@
+package com.android.smartlock.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.android.smartlock.R;
+
+public class ApproveUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approve_users);
+    }
+}
