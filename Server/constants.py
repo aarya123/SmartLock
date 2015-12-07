bf91b1251acacfe1a00931d932c1b3f8f2e69201
green131 <green131@purdue.edu>
2015-12-07 18:25:33 -0500

    Added unapproval process and send all users to admin

--- Server/constants.py
+++ Server/constants.py
@@ -2,8 +2,11 @@
 LOCKED = 1
 UNLOCKED = 0
 
-ADMIN_SUCCESS_MSG = 'You have been added as an admin.'
+ADMIN_SUCCESS_MSG = 'You have been added as an admin'
 APPROVAL_FAILED_MSG = 'Unable to approve user!'
 ALREADY_REGISTERED_MSG = 'You are already registered with this lock'
-REGISTRATION_PENDING_MSG = 'Registration pending approval.'
+NEW_PENDING_USER_MSG = 'A new user has requested lock access'
+REGISTRATION_PENDING_MSG = 'Registration pending approval'
 REGISTRATION_SUCCESS_MSG = 'Registration approved!'
+REGISTRATION_UNAPPROVED_MSG = 'You have been deregistered'
+UNAPPROVAL_FAILED_MSG = 'Failed to unapprove user!'
